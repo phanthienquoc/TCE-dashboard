859b0bd2ef19f14291335b5a3bf5167d0d44fcc3
Phan Thiên Quốc <39411017+phanthienquoc@users.noreply.github.com>
2026-08-22 04:44:57 +0700

    feat: register MFA encryption service

--- apps/service/src/auth/auth.module.ts
+++ apps/service/src/auth/auth.module.ts
@@ -5,8 +5,9 @@ import { AuthService } from './auth.service';
 import { AuthRepository } from './auth.repository';
 import { JwtService } from './jwt.service';
 import { MfaService } from './mfa.service';
+import { MfaCryptoService } from './mfa-crypto.service';
 import { PasswordService } from './password.service';
 import { RefreshService } from './refresh.service';
 
-@Module({ controllers: [AuthController, MfaController], providers: [AuthService, AuthRepository, JwtService, MfaService, PasswordService, RefreshService], exports: [AuthService, AuthRepository, JwtService, MfaService, PasswordService, RefreshService] })
+@Module({ controllers:[AuthController,MfaController], providers:[AuthService,AuthRepository,JwtService,MfaService,MfaCryptoService,PasswordService,RefreshService], exports:[AuthService,AuthRepository,JwtService,MfaService,MfaCryptoService,PasswordService,RefreshService] })
 export class AuthModule {}
