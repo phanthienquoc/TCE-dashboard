d7fe758e203d20d8406e17ce56401aeef2d65554
Phan Thiên Quốc <39411017+phanthienquoc@users.noreply.github.com>
2026-08-22 04:34:40 +0700

    feat: wire JWT refresh and MFA modules

--- apps/service/src/auth/auth.module.ts
+++ apps/service/src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { MfaController } from './mfa.controller';
 import { AuthService } from './auth.service';
+import { JwtService } from './jwt.service';
 import { MfaService } from './mfa.service';
 import { PasswordService } from './password.service';
+import { RefreshService } from './refresh.service';
 
-@Module({ controllers: [AuthController], providers: [AuthService, MfaService, PasswordService], exports: [AuthService, MfaService, PasswordService] })
+@Module({ controllers: [AuthController, MfaController], providers: [AuthService, JwtService, MfaService, PasswordService, RefreshService], exports: [AuthService, JwtService, MfaService, PasswordService, RefreshService] })
 export class AuthModule {}
