40fade9bbe7bef9f98846b1a3487271a967bc079
Phan Thiên Quốc <39411017+phanthienquoc@users.noreply.github.com>
2026-08-22 04:29:36 +0700

    feat: wire auth security services

--- apps/service/src/auth/auth.module.ts
+++ apps/service/src/auth/auth.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { MfaService } from './mfa.service';
+import { PasswordService } from './password.service';
 
-@Module({ controllers: [AuthController] })
+@Module({ controllers: [AuthController], providers: [AuthService, MfaService, PasswordService], exports: [AuthService, MfaService, PasswordService] })
 export class AuthModule {}
