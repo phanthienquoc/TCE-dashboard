b5a897ae26cc6f7ff317a4f21666ada3c136aa05
Phan Thiên Quốc <39411017+phanthienquoc@users.noreply.github.com>
2026-08-22 11:29:14 +0700

    chore: trigger staging deploy from BE change

--- apps/service/src/main.ts
+++ apps/service/src/main.ts
@@ -3,6 +3,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log('[TCE][BE] staging deploy trigger');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
