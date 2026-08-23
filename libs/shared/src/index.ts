f41e32b119b8355f41e230cd76c0f9a07699d315
Phan Thiên Quốc <39411017+phanthienquoc@users.noreply.github.com>
2026-08-23 23:37:13 +0700

    refactor(nx): add shared library boundary

---
+++ libs/shared/src/index.ts
@@ -0,0 +1,4 @@
+export type TceId = string;
+export type TceEnvironment = 'development' | 'staging' | 'production';
+
+export const TCE_ENVIRONMENTS: readonly TceEnvironment[] = ['development', 'staging', 'production'];
