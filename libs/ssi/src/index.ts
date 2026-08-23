f1c3eedf684368a6670fd5d9ca5a3997dc005534
Phan Thiên Quốc <39411017+phanthienquoc@users.noreply.github.com>
2026-08-23 23:57:18 +0700

    refactor(ssi): export broker adapter

--- libs/ssi/src/index.ts
+++ libs/ssi/src/index.ts
@@ -1,12 +1,3 @@
-export type SsiCredentials = {
-  apiKey: string;
-  apiSecret: string;
-  clientId?: string;
-  privateKey?: string;
-  accountNo: string;
-};
-
-export type SsiAuthInput = {
-  otp?: string;
-  transactionId?: string;
-};
+export type SsiCredentials = { apiKey: string; apiSecret: string; clientId?: string; privateKey?: string; accountNo: string };
+export type SsiAuthInput = { otp?: string; transactionId?: string };
+export * from './ssi.broker.adapter';
