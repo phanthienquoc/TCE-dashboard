59cf62ee84efeb2a0fc82491c74dd35a9614329c
Phan Thiên Quốc <39411017+phanthienquoc@users.noreply.github.com>
2026-08-26 15:26:10 +0700

    fix: register TCE account repository token

--- libs/contracts/src/tokens.ts
+++ libs/contracts/src/tokens.ts
@@ -1,5 +1,6 @@
 export const CONTRACT_TOKENS = {
   credentials: Symbol.for('tce.contracts.credentials'),
+  tceAccountRepository: Symbol.for('tce.contracts.tceAccountRepository'),
   positionRepository: Symbol.for('tce.contracts.positionRepository'),
   orderRepository: Symbol.for('tce.contracts.orderRepository'),
   marketData: Symbol.for('tce.contracts.marketData'),
