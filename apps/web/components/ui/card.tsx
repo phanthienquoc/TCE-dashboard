86b38c092e0dd481e2be8fdeea3310a67bdfafec
Phan Thiên Quốc <39411017+phanthienquoc@users.noreply.github.com>
2026-08-26 08:56:53 +0700

    feat(ui): add shadcn card primitive

---
+++ apps/web/components/ui/card.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { cn } from '../../lib/utils';
+
+export const Card=React.forwardRef<HTMLDivElement,React.HTMLAttributes<HTMLDivElement>>(({className,...props},ref)=><div ref={ref} className={cn('rounded-xl border border-white/[0.08] bg-white/[0.035] shadow-xl shadow-black/10',className)} {...props}/>);
+Card.displayName='Card';
+export const CardHeader=React.forwardRef<HTMLDivElement,React.HTMLAttributes<HTMLDivElement>>(({className,...props},ref)=><div ref={ref} className={cn('flex flex-col gap-1.5 p-4 sm:p-5',className)} {...props}/>);
+CardHeader.displayName='CardHeader';
+export const CardTitle=React.forwardRef<HTMLHeadingElement,React.HTMLAttributes<HTMLHeadingElement>>(({className,...props},ref)=><h3 ref={ref} className={cn('font-semibold tracking-tight text-slate-100',className)} {...props}/>);
+CardTitle.displayName='CardTitle';
+export const CardDescription=React.forwardRef<HTMLParagraphElement,React.HTMLAttributes<HTMLParagraphElement>>(({className,...props},ref)=><p ref={ref} className={cn('text-xs text-slate-500',className)} {...props}/>);
+CardDescription.displayName='CardDescription';
+export const CardContent=React.forwardRef<HTMLDivElement,React.HTMLAttributes<HTMLDivElement>>(({className,...props},ref)=><div ref={ref} className={cn('p-4 pt-0 sm:p-5 sm:pt-0',className)} {...props}/>);
+CardContent.displayName='CardContent';
