c0fbc49ffba4957fa7a1b10cdc1b80ccafb87717
Phan Thiên Quốc <39411017+phanthienquoc@users.noreply.github.com>
2026-08-26 15:25:53 +0700

    fix: export TCE account repository

--- libs/db/src/index.ts
+++ libs/db/src/index.ts
@@ -1,4 +1,5 @@
 export type TceAccountKey = { accountId: string };
 export * from './supabase.credentials.adapter';
+export * from './supabase.account.adapter';
 export * from './supabase.position.adapter';
 export * from './supabase.order.adapter';
