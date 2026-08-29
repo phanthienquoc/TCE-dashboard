402c92a806065689ffccb551135e4ae707966e71
Phan Thiên Quốc <39411017+phanthienquoc@users.noreply.github.com>
2026-08-29 10:55:47 +0700

    refactor(web): add SSI platform config renderer

---
+++ apps/web/components/config/platforms/SSIPlatformConfig.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import type { PlatformConfigProps } from './types';
+import SSIPlatform from '../../../app/components/platforms/SSIPlatform';
+
+export default function SSIPlatformConfig({}: PlatformConfigProps) {
+  return <SSIPlatform />;
+}
