d367267ed7a6ae2740cfaba44673c1cd26fa0a84
Phan Thiên Quốc <39411017+phanthienquoc@users.noreply.github.com>
2026-09-09 06:54:47 +0700

    fix(service): import auth module for system updates jwt

--- apps/service/src/system-updates/system-updates.module.ts
+++ apps/service/src/system-updates/system-updates.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { SystemUpdatesController } from './system-updates.controller';
 import { SystemUpdatesService } from './system-updates.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [SystemUpdatesController],
   providers: [SystemUpdatesService],
   exports: [SystemUpdatesService],
