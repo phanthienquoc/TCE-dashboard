598de34acc8367dd14df00f46a0eef599d600185
Phan Thiên Quốc <39411017+phanthienquoc@users.noreply.github.com>
2026-09-16 15:15:22 +0700

    feat: register dividend sync progress controller

--- apps/service/src/stock-events/stock-events.module.ts
+++ apps/service/src/stock-events/stock-events.module.ts
@@ -8,10 +8,11 @@ import { StockDividendPoolController } from './stock-dividend-pool.controller';
 import { StockDividendPoolService } from './stock-dividend-pool.service';
 import { HuntingDividendScannerService } from './hunting-dividend-scanner.service';
 import { DividendOhlcvService } from './dividend-ohlcv.service';
+import { DividendOhlcvController } from './dividend-ohlcv.controller';
 
 @Module({
   imports: [AuthModule, PlatformCredentialsModule],
-  controllers: [StockEventsController, StockDividendPoolController],
+  controllers: [StockEventsController, StockDividendPoolController, DividendOhlcvController],
   providers: [StockEventsService, StockEventsSupabaseRepository, StockDividendPoolService, HuntingDividendScannerService, DividendOhlcvService],
   exports: [StockEventsService, StockDividendPoolService, HuntingDividendScannerService, DividendOhlcvService],
 })
